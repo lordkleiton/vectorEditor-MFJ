06c871272a47cb7516da36f5e64d9b008b601904
lordkleiton <m_s_r@outlook.com>
2019-04-28 02:48:11 -0300

    adding different colors to different vectors

--- assets/script.js
+++ assets/script.js
@@ -5,123 +5,119 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
 //eventos do canvas
-canvas.addEventListener('mousemove', drawToCurrentMousePosition);
 canvas.addEventListener('click', click);
 canvas.addEventListener('keydown', stopDrawing);
+canvas.addEventListener('mousemove', drawToCurrentMousePosition);
 
-//cor de preenchimento
-ctx.fillStyle = "#000";
+ctx.fillStyle = "black";					//cor de preenchimento
 
-//array de posições
-let pos = [];
+let pos = [];								//array de posições
+let draw = true;							//controlando o estado de desenhar
+let div = 50;								//divisor / multiplicador
+let colors = ['800ecb', 'b331b2', 'a4bc13', 'e05886', '0a1e55', '1498e1', '1e2ee2', '60e2dc', '40b40e', 'df5b0b'];
 
-//controlando o estado de desenhar
-let draw = true;
+drawGrid();									//iniciando com grid
 
-//inciando com grid
-drawGrid();
+function click(e){							//clique do mouse
+	draw = (pos.length < 10) ? true : false;
 
-//para de desenhar
-function stopDrawing(e){
-	if (e.key === 'Escape')
-		draw = false;
+	if (draw){
+		let x = Math.trunc(e.clientX / div) * div;
+		let y = Math.trunc(e.clientY / div) * div;
+	
+		ctx.beginPath();
+		ctx.arc(x, y, 5, 0, 2 * Math.PI);
+		ctx.fill();
+		pos.push([x, y]);
+	
+		refresh();
+	}
 }
 
-//imprime posição do mouse
-function drawToCurrentMousePosition(e){
-	clearCanvas(ctx, canvas);
-	redraw();
+function stopDrawing(e){					//para de desenhar
+	if (e.key === 'Escape') draw = false;
+}
+
+function drawToCurrentMousePosition(e){		//imprime posição do mouse
+	refresh();
 
 	if (draw){
 		let l = pos.length;
 	
 		if (l > 0){
+			let x = Math.trunc(e.clientX / div) * div;
+			let y = Math.trunc(e.clientY / div) * div;
+
 			ctx.beginPath();
 			ctx.moveTo(pos[l-1][0], pos[l-1][1]);
-			ctx.lineTo(Math.trunc(e.clientX / 50) * 50, Math.trunc(e.clientY / 50) * 50);
+			ctx.lineTo(x, y);
 			ctx.stroke();
 		}
 	}
 }
 
-//clique do mouse
-function click(e){
-	draw = true;
-
-	ctx.beginPath();
-	ctx.arc(Math.trunc(e.clientX / 50) * 50, Math.trunc(e.clientY / 50) * 50, 5, 0, 2 * Math.PI);
-	ctx.fill();
-	ctx.stroke();
-	pos.push([Math.trunc(e.clientX / 50) * 50, Math.trunc(e.clientY / 50) * 50]);
-
-	redraw();
-}
-
-//limpa o canvas
-function clearCanvas(ctx, canvas){
+function clearCanvas(){						//limpa o canvas
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.beginPath();
 }
 
-//redesenha os pontos 
-function redraw(){
-	drawGrid();
-
-	pos.forEach((p) => {
-		ctx.beginPath();
-		ctx.arc(p[0], p[1], 5, 0, 2 * Math.PI);
-		ctx.fill();
-		ctx.stroke();
-	});
-
+function redraw(){							//redesenha os pontos 
 	for (let i = 0; i < (pos.length - 1); i++){
+		ctx.strokeStyle = `#${colors[i]}`;
 		ctx.beginPath();
 		ctx.moveTo(pos[i][0], pos[i][1]);
 		ctx.lineTo(pos[i + 1][0], pos[i + 1][1]);
 		ctx.stroke();
 	}
 
-	drawResultLine();
-
-	ctx.strokeStyle = "#000";
-}
-
-//desenha a linha da soma vetorial
-function drawResultLine(){
-	if (pos.length > 2){
-		ctx.strokeStyle = "red";
-
+	pos.forEach((p) => {
 		ctx.beginPath();
-		ctx.moveTo(pos[0][0], pos[0][1]);
-		ctx.lineTo(pos[pos.length - 1][0], pos[pos.length - 1][1]);
-		ctx.stroke();
-	}
+		ctx.arc(p[0], p[1], 5, 0, 2 * Math.PI);
+		ctx.fill();
+	});
+
+	if (pos.length > 2) drawResultLine();
+
+	ctx.strokeStyle = "black";
 }
 
-//limpa tudo
-function reset(){
+function drawResultLine(){					//desenha a linha da soma vetorial
+	ctx.strokeStyle = "red";
+
+	ctx.beginPath();
+	ctx.moveTo(pos[0][0], pos[0][1]);
+	ctx.lineTo(pos[pos.length - 1][0], pos[pos.length - 1][1]);
+	ctx.stroke();
+}
+
+function reset(){							//limpa tudo
 	pos = [];
-	clearCanvas(ctx, canvas);
+	clearCanvas();
 	drawGrid();
 }
 
-//desenha o grid
-function drawGrid(){
+function refresh(){							//atualiza a tela
+	clearCanvas();
+	drawGrid();
+	redraw();
+}
+
+function drawGrid(){						//desenha o grid
 	ctx.strokeStyle = "#ccc";
 
-	for (let i = 0; i < canvas.width / 50; i++){
+	for (let i = 0; i < canvas.width / div; i++){
 		//vertical
-		ctx.moveTo(i * 50, 0);
-		ctx.lineTo(i * 50, canvas.height);
+		ctx.moveTo(i * div, 0);
+		ctx.lineTo(i * div, canvas.height);
 		ctx.stroke();
 
 		//horizontal
-		ctx.moveTo(0, i * 50, 0);
-		ctx.lineTo(canvas.width, i * 50);
+		ctx.moveTo(0, i * div, 0);
+		ctx.lineTo(canvas.width, i * div);
 		ctx.stroke();
 	}
 
-	ctx.strokeStyle = "#000";
+	ctx.strokeStyle = "black";
 }
 
 /* 
