960b408e8ba19c984a87326d22cbe7ec915dcf8b
lordkleiton <m_s_r@outlook.com>
2019-04-28 01:27:24 -0300

    added grid

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,9 @@ let pos = [];
 //controlando o estado de desenhar
 let draw = true;
 
+//inciando com grid
+drawGrid();
+
 //para de desenhar
 function stopDrawing(e){
 	if (e.key === 'Escape')
@@ -34,7 +37,7 @@ function drawToCurrentMousePosition(e){
 	
 		if (l > 0){
 			ctx.moveTo(pos[l-1][0], pos[l-1][1]);
-			ctx.lineTo(e.clientX, e.clientY);
+			ctx.lineTo(Math.trunc(e.clientX / 50) * 50, Math.trunc(e.clientY / 50) * 50);
 			ctx.stroke();
 		}
 	}
@@ -45,10 +48,10 @@ function click(e){
 	draw = true;
 
 	ctx.beginPath();
-	ctx.arc(e.clientX, e.clientY, 5, 0, 2 * Math.PI);
+	ctx.arc(Math.trunc(e.clientX / 50) * 50, Math.trunc(e.clientY / 50) * 50, 5, 0, 2 * Math.PI);
 	ctx.fill();
 	ctx.stroke();
-	pos.push([e.clientX, e.clientY, Math.trunc(e.clientX / 100),Math.trunc(e.clientY / 100)]);
+	pos.push([Math.trunc(e.clientX / 50) * 50, Math.trunc(e.clientY / 50) * 50]);
 
 	redraw();
 }
@@ -61,6 +64,8 @@ function clearCanvas(ctx, canvas){
 
 //redesenha os pontos 
 function redraw(){
+	drawGrid();
+
 	pos.forEach((p) => {
 		ctx.beginPath();
 		ctx.arc(p[0], p[1], 5, 0, 2 * Math.PI);
@@ -74,22 +79,45 @@ function redraw(){
 		ctx.stroke();
 	}
 
+	ctx.strokeStyle = "red";
+
 	if (pos.length > 2){
 		ctx.moveTo(pos[0][0], pos[0][1]);
 		ctx.lineTo(pos[pos.length - 1][0], pos[pos.length - 1][1]);
 		ctx.stroke();
 	}
+
+	ctx.strokeStyle = "#000";
 }
 
 //limpa tudo
 function reset(){
 	pos = [];
 	clearCanvas(ctx, canvas);
+	drawGrid();
 }
 
+function drawGrid(){
+	ctx.strokeStyle = "#ccc";
 
-function drawLine(t){
-	if (t === 1){
-
+	for (let i = 0; i < canvas.width / 50; i++){
+		ctx.moveTo(i * 50, 0);
+		ctx.lineTo(i * 50, canvas.height);
+		ctx.stroke();
 	}
+	for (let i = 0; i < canvas.height / 50; i++){
+		ctx.moveTo(0, i * 50, 0);
+		ctx.lineTo(canvas.width, i * 50);
+		ctx.stroke();
+	}
+
+	ctx.fill();
+
+	ctx.strokeStyle = "#000";
 }
+
+/* 
+
+modulo = Math.sqrt( Math.pow(Xb - Xa, 2) + Math.pow(Yb - Ya, 2) )
+
+*/
